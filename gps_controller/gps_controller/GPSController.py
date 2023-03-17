d472a902f7ae49fb91d5f3efd4afdd6bbd5fc23e
Quinn Cypher <tmqCypher.git@gmail.com>
2023-03-17 13:44:00 -0400

    Sprint 2 submission

--- gps_controller/gps_controller/GPSController.py
+++ gps_controller/gps_controller/GPSController.py
@@ -1,5 +1,5 @@
-import time
 import serial
+# TODO: Find a better u-blox library. This one is really slow + out of date
 from ublox_gps import UbloxGps
 
 import rclpy
@@ -9,27 +9,37 @@ from gps_interfaces.srv import Coordinates
 
 
 class GPSController(Node):
+    '''Provides a ROS2 interface to a Sparkfun u-blox GPS module
 
+    GPSController provides a service that responds to clients with the
+    latitude and longitude read from the GPS module.
+    '''
+ 
     def __init__(
             self,
             serial_port: str = '/dev/serial0',
             baudrate: int = 38400,
-            use_cached_values: bool = False,
-            cache_update_interval: float = 1.0):
+            #use_cached_values: bool = False,
+            #cache_update_interval: float = 1.0):
+            ):
         super().__init__('gps_controller', parameter_overrides=[])
 
+        self._coord_srv = self.create_service(Coordinates, 'coordinates', self._srv_callback)
+
+        '''Cached coordinate retrieval currently unimplemented
         self._coord_srv = self.create_service(Coordinates, 'coordinates',
-                self._cached_srv_callback if use_cached_values else self._srv_callback)
+            self._cached_srv_callback if use_cached_values else self._srv_callback)
+            
         if use_cached_values:
             self._timer = self.create_timer(cache_update_interval, self._cache_update_callback)
+        '''
 
-        self._serial = serial.Serial(serial_port, baudrate)
-        self._gps = UbloxGps(serial.Serial('/dev/serial0', baudrate=38400, timeout=1))
+        self._gps = UbloxGps(serial.Serial('/dev/serial0', baudrate=baudrate, timeout=1))
 
         self.log = self.get_logger()
         self.log.info('Initialized')
-        
 
+    '''Cached coordinate retrieval currently unimplemented
     def _cache_update_callback(self):
         geo = self._gps.geo_coords()
         self._cached_lat = geo.lat
@@ -39,6 +49,7 @@ class GPSController(Node):
         response.lat = self._cached_lat
         response.lon = self._cached_lon
         return response
+    '''
 
     def _srv_callback(self, request, response):
         geo = self._gps.geo_coords()
